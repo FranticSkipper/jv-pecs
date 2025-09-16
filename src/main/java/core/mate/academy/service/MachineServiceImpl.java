c72eeaad065774a102c2e566f3f40e6cd76466ae
Oleg <oleg@gmail.com>
2025-09-16 15:18:57 +0300

    implement MachineService class and Machine hierarchy with pecs

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,42 @@
 package core.mate.academy.service;
 
-/**
- * Your implementation of MachineService.
- */
-public class MachineServiceImpl {
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
+    private final BulldozerProducer bulldozerProducer = new BulldozerProducer();
+    private final ExcavatorProducer excavatorProducer = new ExcavatorProducer();
+    private final TruckProducer truckProducer = new TruckProducer();
+
+    @Override
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        List<Machine> machines = new ArrayList<>();
+
+        if (type == Bulldozer.class) {
+            machines.addAll(this.bulldozerProducer.get());
+        } else if (type == Excavator.class) {
+            machines.addAll(this.excavatorProducer.get());
+        } else if (type == Truck.class) {
+            machines.addAll(this.truckProducer.get());
+        }
+
+        return machines;
+    }
+
+    @Override
+    public void fill(List<? super T> machines, T value) {
+        Collections.fill(machines, value);
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine e: machines) {
+            e.doWork();
+        }
+    }
 }
