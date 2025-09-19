e9e62e8939f4374d27bf3c1521b11d65a23f7567
Oleg <oleg@gmail.com>
2025-09-19 18:52:24 +0300

    fix: fix types, implement mentor's changes

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -8,32 +8,30 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
+public class MachineServiceImpl implements MachineService<Machine> {
     private final BulldozerProducer bulldozerProducer = new BulldozerProducer();
     private final ExcavatorProducer excavatorProducer = new ExcavatorProducer();
     private final TruckProducer truckProducer = new TruckProducer();
 
     @Override
-    public List<T> getAll(Class<? extends T> type) {
+    public List<Machine> getAll(Class<? extends Machine> type) {
         if (type == Bulldozer.class) {
-            return (List<T>) new ArrayList<>(this.bulldozerProducer.get());
+            return new ArrayList<>(this.bulldozerProducer.get());
         } else if (type == Excavator.class) {
-            return (List<T>) new ArrayList<>(this.excavatorProducer.get());
+            return new ArrayList<>(this.excavatorProducer.get());
         } else if (type == Truck.class) {
-            return (List<T>) new ArrayList<>(this.truckProducer.get());
+            return new ArrayList<>(this.truckProducer.get());
         }
         return new ArrayList<>();
     }
 
     @Override
-    public void fill(List<? super T> machines, T value) {
+    public void fill(List<? super Machine> machines, Machine value) {
         Collections.fill(machines, value);
     }
 
     @Override
     public void startWorking(List<? extends Machine> machines) {
-        for (Machine e: machines) {
-            e.doWork();
-        }
+        machines.forEach(Machine::doWork);
     }
 }
